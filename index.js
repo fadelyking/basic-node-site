d8986955696e4b8d43b3f34c8e3d0ee046cefeb2
fadelyking <fadely195@gmail.com>
2023-12-06 13:58:30 +0300

    established a route to 3 different pages, index.html about.html contact-me.html 404.html

--- index.js
+++ index.js
@@ -1,3 +1,58 @@
 const url = require("url");
 const http = require("http");
 const fs = require("fs");
+
+http.createServer(function (req, res) {
+	const q = url.parse(req.url, true);
+	const filename = "." + q.pathname;
+	if (q.pathname === "/") {
+		fs.readFile("index.html", function (err, data) {
+			if (err) {
+				fs.readFile("404.html", function (err, data) {
+					if (err) throw err;
+					res.writeHead(200, { "Content-Type": "text/html" });
+					res.write(data);
+					return res.end();
+				});
+			}
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.write(data);
+			return res.end();
+		});
+	} else if (q.pathname === "/about") {
+		fs.readFile("about.html", function (err, data) {
+			if (err) {
+				fs.readFile("404.html", function (err, data) {
+					if (err) throw err;
+					res.writeHead(200, { "Content-Type": "text/html" });
+					res.write(data);
+					return res.end();
+				});
+			}
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.write(data);
+			return res.end();
+		});
+	} else if (q.pathname === "/contact-me") {
+		fs.readFile("contact-me.html", function (err, data) {
+			if (err) {
+				fs.readFile("404.html", function (err, data) {
+					if (err) throw err;
+					res.writeHead(200, { "Content-Type": "text/html" });
+					res.write(data);
+					return res.end();
+				});
+			}
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.write(data);
+			return res.end();
+		});
+	} else {
+		fs.readFile("404.html", function (err, data) {
+			if (err) throw err;
+			res.writeHead(200, { "Content-Type": "text/html" });
+			res.write(data);
+			return res.end();
+		});
+	}
+}).listen(8080);
