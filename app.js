0519b500b39d62b19bc7552e221aa2e55887b45c
fadelyking <fadely195@gmail.com>
2023-12-10 10:09:23 +0300

    refactored code to work with express

---
+++ app.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.get("/about", (req, res) => {
+	res.sendFile(path.join(__dirname, "/about.html"));
+});
+
+app.get("/contact-me", (req, res) => {
+	res.sendFile(path.join(__dirname, "/contact-me.html"));
+});
+
+app.use((req, res) => {
+	res.status(404).sendFile(path.join(__dirname, "/404.html"));
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}!`);
+});
